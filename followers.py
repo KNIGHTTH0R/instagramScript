9bbb793731ea04e51ef61d86bc9d18d6ef411b55
Juan Cruz <jcbasso95@gmail.com>
2017-03-09 00:01:01 -0300

    Ya no logueo muchas veces, sino una sola

--- followers.py
+++ followers.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 # Use text editor to edit the script and type in valid Instagram username/password
-
+from InstagramAPI import InstagramAPI	
 import json
 import contextlib
 import io
 import sys
 import threading
 
-first = True
 # esta funcion hace que no se vea el stdout cuando corro una funcion
 @contextlib.contextmanager
 def nostdout():
@@ -18,21 +17,11 @@ def nostdout():
     yield
     sys.stdout = save_stdout
 
-def followers():
-	from InstagramAPI import InstagramAPI
-	global first
+def followers(otherusername,fileName="followers",time=60):
+	text_file = open(fileName, "a")
 
-	text_file = open("followers", "a")
-	time = 60 # segundos
-	threading.Timer(time, followers).start() # crea un thread cada "time" tiempo 
+	threading.Timer(time, followers, [otherusername,fileName,time]).start() # crea un thread cada "time" tiempo 
 
-	yourusername = "" # username/password de cuenta ig con la loguearas
-	yourpassword = ""
-	InstagramAPI = InstagramAPI(yourusername,yourpassword) 
-	with nostdout():
-		InstagramAPI.login() # login
-	
-	otherusername = "" # @usuario al cual se buscara
 	InstagramAPI.searchUsername(otherusername) 
 	usuario = InstagramAPI.LastJson # devuelve la respuesta JSON de la funcion anterior
 
@@ -43,8 +32,23 @@ def followers():
 	s = ""
 	s += "\n"
 	s += "Cantidad de followers de " + str(name) + " (@" + str(username) + ") : " + str(followerCount)
-	first = False
 	text_file.write("%s" % s)
 	text_file.close()
 
-followers()
+#MAIN:
+#Valores a completar
+yourusername = "jcwithc" # COMPLETAR CON username/password de cuenta ig con la loguearas. Ej: jcwithc
+yourpassword = "papalo" # Ej: pepe
+otherusername = "pero" # COMPLETAR CON usuario al cual se buscara. Ej: jcwithc
+fileName = "followers" # Nombre del archivo donde se guardaran los followers. Ej: followers
+time = 5 # Segundos cada cuantos se crea un nuevo thread
+
+InstagramAPI = InstagramAPI(yourusername,yourpassword) 
+with nostdout(): # Sirve para borrar el error del stdout de login de la API
+	ok = InstagramAPI.login() # login
+if ok:
+	print "Login success as " + yourusername + "!"
+	print "Followers will be written in \"" + fileName + "\" file"
+	followers(otherusername,fileName,time)
+else:
+	print "Login error!"
